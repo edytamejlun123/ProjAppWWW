d5b961202997af7732d983e45921a65c4571b6fe
edytamejlun_ <92027759+edytamejlun123@users.noreply.github.com>
2024-01-08 17:42:44 +0100

    trgrt

---
+++ lab8/html/strona1.php
@@ -0,0 +1,7 @@
+
+
+				<img src="../img/kingFahd.jpg"> <br>King Fahd Causeway <br>
+				<img src="../img/jintang.jpg"> <br>	Jintang Bridge <br>
+				<img src="../img/AtchafalayaBasinBridge.jpg"> <br> Atchafalaya Basin Bridge <br>
+				<img src="../img/MostDonghai.jpg"> <br> Most Donghai <br>
+				<img src="../img/MostRunyang.jpg"> <br> Most Runyang <br>
---
+++ lab8/html/strona5.php
@@ -0,0 +1,5 @@
+<form action="167363@student.uwm.edu.pl" method="post">
+<label for="wiadomosc">Wpisz wiadomość:</label><br>
+<textarea id="wiadomosc" name="body" rows="4" cols="50"></textarea><br><br>
+<input type="submit" value="Wyślij">
+</form>
